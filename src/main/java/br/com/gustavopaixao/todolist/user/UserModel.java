447d1e2cb2fdac8c64a43057b2b387cf5719335c
gustavopaixa1 <luizgustavopaixa1@gmail.com>
2023-10-11 07:35:26 -0300

    Adicionando o repositório do projeto. Aula 2/5.

---
+++ src/main/java/br/com/gustavopaixao/todolist/user/UserModel.java
@@ -0,0 +1,30 @@
+package br.com.gustavopaixao.todolist.user;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.Data;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Data
+@Entity(name = "tb_users")
+public class UserModel {
+
+    //Cria um id UUID automaticamente com o generatedValue
+    @Id
+    @GeneratedValue(generator = "UUID")
+    private UUID id;
+
+    @Column(unique = true)
+    private String username;
+    private String name;
+    private String password;
+
+    //Quando o dado for gerado, o banco de dados saberá
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+}
